7655e60d0247df427d95c3ab3977e9be2864be01
Takafumi Arakaki <aka.tkf@gmail.com>
2012-06-11 19:50:19 +0200

    Change the base class of three+

--- ne2wm-pst-three+.el
+++ ne2wm-pst-three+.el
@@ -63,7 +63,7 @@
 (e2wm:pst-class-register
  (make-e2wm:$pst-class
   :name   'three+
-  :extend 'base
+  :extend 'two+
   :title  "Three"
   :init   'ne2wm:dp-three+-init
   :start  'ne2wm:dp-three+-start
